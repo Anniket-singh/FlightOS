3c3cdac888e7798a3eeb73cbe0230f9b9591e601
Anniket-singh <aniketsingh92113@gmail.com>
2026-09-07 14:02:38 +0530

    feat: establish database foundation

--- backend/flightos-backend/src/main/java/com/flightos/FlightosBackendApplication.java
+++ backend/flightos-backend/src/main/java/com/flightos/FlightosBackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlightosBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightosBackendApplication.class, args);
+		SpringApplication.run(FlightosBackendApplication.class, args
+		);
 	}
 
 }
